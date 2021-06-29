674df0673ceb8926cbeddd60eadc7e916a955410
Zubaer <bijon7@yahoo.com>
2021-06-29 12:13:43 -0700

    object files

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+let fruit = "orange";
+let n = 6;
+
+if (n > 5)
+{console.log ("The fruit name has more than five characters.")
+
+} 
+else
+
+{console.log("The fruit name has five characters or less.")};
